662cccf7eeb11824bbb1942f88762088746f1be6
yoonjongyoon <yjy0503@yonsei.ac.kr>
2023-11-20 02:44:45 +0900

    feat: [UC19] Implement of APIFoodDiaryController.java, FoodDiaryService.java

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIFoodDiaryController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIFoodDiaryController.java
@@ -8,4 +8,25 @@ import java.util.List;
 
 public class APIFoodDiaryController extends APIController {
 
+    @Autowired
+    private FoodDiaryService foodDiaryService;
+
+    @GetMapping
+    public ResponseEntity<List<FoodDiaryEntry>> getAllEntries() {
+        return ResponseEntity.ok(foodDiaryService.getAllEntries());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FoodDiaryEntry> getEntryById(@PathVariable Long id) {
+        FoodDiaryEntry entry = foodDiaryService.getEntryById(id);
+        if (entry == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(entry);
+    }
+
+    @PostMapping
+    public ResponseEntity<FoodDiaryEntry> addEntry(@RequestBody FoodDiaryEntry entry) {
+        return ResponseEntity.ok(foodDiaryService.addEntry(entry));
+    }
 }
--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/services/FoodDiaryService.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/services/FoodDiaryService.java
@@ -1,7 +1,10 @@
 package edu.ncsu.csc.iTrust2.services;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +13,8 @@ import edu.ncsu.csc.iTrust2.repositories.FoodDiaryRepository;
 @Component
 @Transactional
 public class FoodDiary extends Service {
+
+    @Autowired
 	private FoodDiaryRepository repository;
 	
 	@Override
@@ -18,4 +23,9 @@ public class FoodDiary extends Service {
     }
 	
 	// find by patient or HCP?
-}
+
+    public List<FoodDiaryEntry> getAllEntries() {return repository.findAllByOrderByDateDesc();}
+
+    public FoodDiaryEntry getEntryById(Long id) {return repository.findById(id).orElse(null);}
+
+    public FoodDiaryEntry addEntry(FoodDiaryEntry entry) {return repository.save(entry);}
