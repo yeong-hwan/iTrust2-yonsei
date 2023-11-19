62fef59b3dc8a808ad903c80aa6b9204fa3ca89f
yeong-hwan <jghff700@naver.com>
2023-11-20 01:20:38 +0900

    feat: [UC19] Implement FoodDiary repository class

---
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/repositories/FoodDiaryRepository.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc.iTrust2.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.ncsu.csc.iTrust2.models.FoodDiary;
+
+public class FoodDiaryRepository extends JpaRepository<FoodDiary, Long> { 
+    public List<FoodDiary> findByHcp ( User hcp );
+
+    public List<FoodDiary> findByPatient ( User patient );
+
+}
