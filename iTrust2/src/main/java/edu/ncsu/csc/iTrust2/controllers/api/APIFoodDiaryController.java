8b19c5b7038193d36978fdad3b5677abadb69923
yoonjongyoon <yjy0503@yonsei.ac.kr>
2023-11-20 02:26:28 +0900

    refactor: [UC19] change FoodDiary.java -> FoodDiaryService.java, add import

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIFoodDiaryController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIFoodDiaryController.java
@@ -1,5 +1,9 @@
 package edu.ncsu.csc.iTrust2.controllers.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
 
 public class APIFoodDiaryController extends APIController {
