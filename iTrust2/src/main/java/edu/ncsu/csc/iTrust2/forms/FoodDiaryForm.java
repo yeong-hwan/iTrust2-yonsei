4a83f72128a5d4854297fb40d0be5f7610b32fed
yeong-hwan <jghff700@naver.com>
2023-11-20 01:38:43 +0900

    refactor: [UC19] Implement Constructors for FoodDiary Form

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/forms/FoodDiaryForm.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/forms/FoodDiaryForm.java
@@ -1,6 +1,15 @@
 package edu.ncsu.csc.iTrust2.forms;
 
+import edu.ncsu.csc.iTrust2.models.FoodDiary;
+
+/**
+ * * A form for REST API communication. Contains fields for constructing
+ * FoodDiary objects.
+ * 
+ * @author jang-yeonghwan
+ */
 public class FoodDiaryForm {
+	
 	private Long id;
 	private String date;
 	private String mealType;
@@ -13,5 +22,33 @@ public class FoodDiaryForm {
 	private Long sugarsPerServing;
 	private Long fiberPerServing;
 	private Long proteinPerServing;
+	
+	/**
+     * Empty constructor for filling in fields without a Drug object.
+     */
+	public FoodDiaryForm() {
+	}
+	
+	/**
+	 * Constructs a new form with information from the given FoodDiary.
+	 * @param foodDiary
+	 */
+	public FoodDiaryForm(final FoodDiary foodDiary) {
+		setId( foodDiary.getId() );
+		setDate( foodDiary.getDate() );
+		setMealType( foodDiary.getMealType() );
+		setFoodName( foodDiary.getFoodName() );
+		setServingNumber( foodDiary.getServingNumber() );
+		setCaloriesPerServing( foodDiary.getCaloriesPerServing() );
+		setFatPerServing( foodDiary.getFatPerServing() );
+		setSodiumPerServing( foodDiary.getSodiumPerServing() );
+		setCarbsPerServing( foodDiary.getCarbsPerServing() );
+		setSugarPerServing( foodDiary.getSugarPerServing() );
+		setFiberPerServing( foodDiary.getFiberPerServing() );
+		setProteinPerServing( foodDiary.getProteinPerServing() );
+	}
+	
 
+		
+		
 }
