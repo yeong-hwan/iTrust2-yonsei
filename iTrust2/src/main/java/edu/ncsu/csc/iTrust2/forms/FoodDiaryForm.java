5504dcd25a715b826e4eca8cead4236c4eae537e
yeong-hwan <jghff700@naver.com>
2023-11-20 01:30:53 +0900

    refactor: [UC19] Add private variables for FoodDiary Form

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/forms/FoodDiaryForm.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/forms/FoodDiaryForm.java
@@ -1,5 +1,17 @@
 package edu.ncsu.csc.iTrust2.forms;
 
 public class FoodDiaryForm {
+	private Long id;
+	private String date;
+	private String mealType;
+	private String foodName;
+	private Long servingNumber;
+	private Long caloriesPerServing;
+	private Long fatPerServing;
+	private Long sodiumPerServing;
+	private Long carbsPerServing;
+	private Long sugarsPerServing;
+	private Long fiberPerServing;
+	private Long proteinPerServing;
 
 }
