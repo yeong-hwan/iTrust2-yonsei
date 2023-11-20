7091c205c9ba1667139293fd7ceb2e9d6efe6ea0
Yewon Lim <ga06033@yonsei.ac.kr>
2023-11-20 23:28:04 +0900

    fix: upper-lower; i hate ncsu

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/repositories/BasicHealthMetricsRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.ncsu.csc.iTrust2.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.ncsu.csc.iTrust2.models.BasicHealthMetrics;
-
-public interface BasicHealthMetricsRepository extends JpaRepository<BasicHealthMetrics, Long> {
-
-}
