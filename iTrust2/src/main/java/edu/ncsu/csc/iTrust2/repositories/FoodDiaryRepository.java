3d554b25e6eb72c3fe473da61839eb56636f622a
yeong-hwan <jghff700@naver.com>
2023-11-20 21:29:48 +0900

    feat: [UC19] Add JPA Query at Food Diary Repository

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/repositories/FoodDiaryRepository.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/repositories/FoodDiaryRepository.java
@@ -1,18 +1,28 @@
 package edu.ncsu.csc.iTrust2.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import edu.ncsu.csc.iTrust2.models.FoodDiary;
+import edu.ncsu.csc.iTrust2.models.User;
 
+/**
+ * This is the repository for interacting with the Food Diary table
+ * - findAllByUsername
+ * - findAllByDate
+ * - findAllByMealType
+ * 
+ * Expecting these functions to be automatsically implemented by Spring JPA.
+ */
+public interface FoodDiaryRepository extends JpaRepository<FoodDiary, Long> {
 
-public interface FoodDiaryRepository extends JpaRepository<FoodDiary, Long> { 
-//    public List<FoodDiary> findByHcp ( User hcp );
-//
-//    public List<FoodDiary> findByPatient ( User patient );
-//
-//    public List<FoodDiaryEntry> findAllByOrderByDateDesc();
-//
-//    public List<FoodDiaryEntry> findAllByDate(Date date);
+  List<FoodDiary> findAllByIdContaining(Long id);
+
+  List<FoodDiary> findAllByDateContaining(Date date);
+
+  List<FoodDiary> findByMealTypeContaining(String mealType);
 }
