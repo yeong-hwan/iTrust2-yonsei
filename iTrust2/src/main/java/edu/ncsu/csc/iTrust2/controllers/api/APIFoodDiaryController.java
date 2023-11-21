eef82633fff0f96e133fb2a5dcb5d212cfc9fa56
Yewon Lim <ga06033@yonsei.ac.kr>
2023-11-21 16:29:59 +0900

    fix: addFoodDiary post API

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIFoodDiaryController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIFoodDiaryController.java
@@ -2,6 +2,7 @@ package edu.ncsu.csc.iTrust2.controllers.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 
@@ -19,6 +20,7 @@ import edu.ncsu.csc.iTrust2.services.UserService;
 import edu.ncsu.csc.iTrust2.utils.LoggerUtil;
 
 @RestController
+@SuppressWarnings ( { "rawtypes", "unchecked" } )
 public class APIFoodDiaryController extends APIController {
 
     @Autowired
@@ -30,9 +32,30 @@ public class APIFoodDiaryController extends APIController {
     @Autowired
     private UserService userService;
 
+    /* @yewon added 11.21
+     * Create a new food diary entry for the patient with the given FoodDiary form
+     */
     @PostMapping(BASE_PATH + "/food_diary/add")
-    public ResponseEntity<FoodDiary> addEntry(@RequestBody FoodDiary entry) {
-        return ResponseEntity.ok(foodDiaryService.addEntry(entry));
+    public ResponseEntity addEntry(@RequestBody FoodDiaryForm entry) {
+        // return ResponseEntity.ok(foodDiaryService.addEntry(entry));
+        // final User self = userService.findByName( LoggerUtil.currentUser() );
+        try{
+            final FoodDiary foodDiary = new FoodDiary(entry);
+            foodDiaryService.save(foodDiary);
+            // =====================================   
+            // !! You should add FOOD_DIARY_ADD to TransactionType.java !!
+            // =====================================   
+            return new ResponseEntity(foodDiary, HttpStatus.OK);
+            // loggerUtil.log( TransactionType.FOOD_DIARY_ADD, LoggerUtil.currentUser(),
+            //         "Added food diary entry for " + entry.getUsername() );
+        } catch ( final Exception e ) {
+            return new ResponseEntity(
+                    errorResponse( "Could not create " + entry.getUsername() + " because of " + e.getMessage() ),
+                    HttpStatus.BAD_REQUEST );
+        }
+        
+        
+
     }
 
     // @GetMapping(BASE_PATH + "food_diary/view")
@@ -48,12 +71,12 @@ public class APIFoodDiaryController extends APIController {
     // return ResponseEntity.ok(f);
     // }
 
-    @GetMapping(BASE_PATH + "food_diary/view/{patientMID}/{date}")
-    public FoodDiary calculateDailyTotal() {
-        final FoodDiary dailyTotal =
-        foodDiaryService.calculateDailyTotal(@PathVariable("patientMID") Long id, @PathVariable("date") Date date);
-    return dailyTotal;
-    }
+    // @GetMapping(BASE_PATH + "food_diary/view/{patientMID}/{date}")
+    // public FoodDiary calculateDailyTotal() {
+    //     final FoodDiary dailyTotal =
+    //     foodDiaryService.calculateDailyTotal(@PathVariable("patientMID") Long id, @PathVariable("date") Date date);
+    // return dailyTotal;
+    // }
 
     // @GetMapping(BASE_PATH + "food_diary/view/{patientMID}/{date}/{mealType}")
     // public ResponseEntity List<FoodDiary> viewEntries(
