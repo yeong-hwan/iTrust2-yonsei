2d35f6f9e7726ee6e56c2a6aefbb79f2b1d9ab17
yeong-hwan <jghff700@naver.com>
2023-11-23 13:45:34 +0900

    test: Implement test for Food Diary Creation by Form

--- iTrust2/src/test/java/edu/ncsu/csc/iTrust2/unit/ICDCodeTest.java
+++ iTrust2/src/test/java/edu/ncsu/csc/iTrust2/unit/ICDCodeTest.java
@@ -23,103 +23,96 @@ import edu.ncsu.csc.iTrust2.models.ICDCode;
  * @author Thomas
  *
  */
-@RunWith ( SpringRunner.class )
+@RunWith(SpringRunner.class)
 @EnableAutoConfiguration
-@SpringBootTest ( classes = TestConfig.class )
+@SpringBootTest(classes = TestConfig.class)
 public class ICDCodeTest {
 
     @Test
     @Transactional
-    public void testCodes () {
+    public void testCodes() {
         final ICDCodeForm form = new ICDCodeForm();
-        form.setId( 1L );
-        form.setCode( "T11" );
-        form.setDescription( "Testing" );
+        form.setId(1L);
+        form.setCode("T11");
+        form.setDescription("Testing");
         final ICDCode base = new ICDCode();
-        base.setCode( "T11" );
-        base.setDescription( "Testing" );
-        base.setId( 1L );
+        base.setCode("T11");
+        base.setDescription("Testing");
+        base.setId(1L);
 
-        final ICDCode code = new ICDCode( form );
-        assertEquals( code, base );
+        final ICDCode code = new ICDCode(form);
+        assertEquals(code, base);
 
-        final ICDCodeForm f2 = new ICDCodeForm( code );
-        assertEquals( form, f2 );
+        final ICDCodeForm f2 = new ICDCodeForm(code);
+        assertEquals(form, f2);
 
-        assertEquals( "T11", code.getCode() );
-        assertTrue( code.getId().equals( 1L ) );
-        assertEquals( "Testing", code.getDescription() );
+        assertEquals("T11", code.getCode());
+        assertTrue(code.getId().equals(1L));
+        assertEquals("Testing", code.getDescription());
 
     }
 
     @Test
     @Transactional
-    public void testInvalidCodes () {
+    public void testInvalidCodes() {
         final ICDCodeForm form = new ICDCodeForm();
-        form.setCode( "111" );
-        form.setDescription( "Invalid" );
+        form.setCode("111");
+        form.setDescription("Invalid");
 
-        @SuppressWarnings ( "unused" ) // we want to do an assignment so that
-                                       // the compiler doesn't optimize it away
+        @SuppressWarnings("unused") // we want to do an assignment so that
+                                    // the compiler doesn't optimize it away
         ICDCode code;
         try {
-            code = new ICDCode( form );
+            code = new ICDCode(form);
             fail();
+        } catch (final IllegalArgumentException e) {
+            assertEquals("Code must begin with a capital letter: 111", e.getMessage());
         }
-        catch ( final IllegalArgumentException e ) {
-            assertEquals( "Code must begin with a capital letter: 111", e.getMessage() );
-        }
-        form.setCode( "AA1" );
+        form.setCode("AA1");
         try {
-            code = new ICDCode( form );
+            code = new ICDCode(form);
             fail();
+        } catch (final IllegalArgumentException e) {
+            assertEquals("Second character of code must be a digit: AA1", e.getMessage());
         }
-        catch ( final IllegalArgumentException e ) {
-            assertEquals( "Second character of code must be a digit: AA1", e.getMessage() );
-        }
-        form.setCode( "A1.A" );
+        form.setCode("A1.A");
         try {
-            code = new ICDCode( form );
+            code = new ICDCode(form);
             fail();
+        } catch (final IllegalArgumentException e) {
+            assertEquals("Third character of code must be alphanumeric: A1.A", e.getMessage());
         }
-        catch ( final IllegalArgumentException e ) {
-            assertEquals( "Third character of code must be alphanumeric: A1.A", e.getMessage() );
-        }
-        form.setCode( "A11A" );
+        form.setCode("A11A");
         try {
-            code = new ICDCode( form );
+            code = new ICDCode(form);
             fail();
+        } catch (final IllegalArgumentException e) {
+            assertEquals("Fourth character of code must be decimal: A11A", e.getMessage());
         }
-        catch ( final IllegalArgumentException e ) {
-            assertEquals( "Fourth character of code must be decimal: A11A", e.getMessage() );
-        }
-        form.setCode( "A11.." );
+        form.setCode("A11..");
         try {
-            code = new ICDCode( form );
+            code = new ICDCode(form);
             fail();
+        } catch (final IllegalArgumentException e) {
+            assertEquals("Characters after decimal must be alphanumeric: A11..", e.getMessage());
         }
-        catch ( final IllegalArgumentException e ) {
-            assertEquals( "Characters after decimal must be alphanumeric: A11..", e.getMessage() );
-        }
-        form.setCode( "A1" );
+        form.setCode("A1");
         try {
-            code = new ICDCode( form );
+            code = new ICDCode(form);
             fail();
+        } catch (final IllegalArgumentException e) {
+            assertEquals("Code must be at least three characters: A1", e.getMessage());
         }
-        catch ( final IllegalArgumentException e ) {
-            assertEquals( "Code must be at least three characters: A1", e.getMessage() );
-        }
-        form.setCode( "A11.1AA1A" );
+        form.setCode("A11.1AA1A");
         try {
-            code = new ICDCode( form );
+            code = new ICDCode(form);
             fail();
+        } catch (final IllegalArgumentException e) {
+            assertEquals("Code too long! Max 8 characters including decimal: A11.1AA1A", e.getMessage());
         }
-        catch ( final IllegalArgumentException e ) {
-            assertEquals( "Code too long! Max 8 characters including decimal: A11.1AA1A", e.getMessage() );
-        }
-        form.setCode( "A11.111A" );
+        form.setCode("A11.111A");
         // valid
-        code = new ICDCode( form );
+        code = new ICDCode(form);
 
     }
 }
