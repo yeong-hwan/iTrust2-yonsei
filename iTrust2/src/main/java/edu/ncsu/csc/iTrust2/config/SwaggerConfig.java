e6b7f108d5c6c69241a7a76b6409571a97bfb522
yeong-hwan <jghff700@naver.com>
2023-11-30 16:51:53 +0900

    chore: Disable swagger dependency

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/config/SwaggerConfig.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/config/SwaggerConfig.java
@@ -1,56 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
+// import springfox.documentation.builders.ApiInfoBuilder;
+// import springfox.documentation.builders.PathSelectors;
+// import springfox.documentation.builders.RequestHandlerSelectors;
+// import springfox.documentation.service.ApiInfo;
+// import springfox.documentation.spi.DocumentationType;
+// import springfox.documentation.spring.web.plugins.Docket;
+// import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+// @Configuration
+// @EnableSwagger2
+// @EnableWebMvc
+// public class SwaggerConfig {
 
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
+// @Bean
+// public Docket api() {
+// return new Docket(DocumentationType.OAS_30) // open api spec 3.0
+// .groupName("groupName")
+// .select()
+// // .apis(RequestHandlerSelectors.basePackage("com.example.swagger"))
+// // .paths(PathSelectors.ant("/api/**"))
+// .apis(RequestHandlerSelectors.any())
+// .paths(PathSelectors.any())
+// .build()
+// .apiInfo(this.apiInfo());
+// }
 
-  private static final String API_NAME = "Study API";
-  private static final String API_VERSION = "0.0.1";
-  private static final String API_DESCRIPTION = "Study API 명세서";
-
-  @Bean
-  public Docket api() {
-    Parameter parameterBuilder = new ParameterBuilder()
-        .name(HttpHeaders.AUTHORIZATION)
-        .description("Access Tocken")
-        .modelRef(new ModelRef("string"))
-        .parameterType("header")
-        .required(false)
-        .build();
-
-    List<Parameter> globalParamters = new ArrayList<>();
-    globalParamters.add(parameterBuilder);
-
-    return new Docket(DocumentationType.SWAGGER_2)
-        .globalOperationParameters(globalParamters)
-        .apiInfo(apiInfo())
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("com.kjh.study.api"))
-        .paths(PathSelectors.any())
-        .build();
-  }
-
-  public ApiInfo apiInfo() {
-    return new ApiInfoBuilder()
-        .title(API_NAME)
-        .version(API_VERSION)
-        .description(API_DESCRIPTION)
-        .build();
-  }
-}
+// private ApiInfo apiInfo() {
+// return new ApiInfoBuilder()
+// .title("Hello Swagger")
+// .description("스웨거 기능 테스트")
+// .version("1.0")
+// .build();
+// }
+// }
